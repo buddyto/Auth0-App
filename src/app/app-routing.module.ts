d4a757c9e8cde16296f8762bf20e32b4be459f43
Gaspar Aufranc <gaspar.aufranc@gmail.com>
2021-06-07 21:05:07 -0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { AuthGuard } from './services/auth.guard';
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'precios', component: PreciosComponent},
+  {path: 'protegida', component: ProtegidaComponent, canActivate: [AuthGuard]},
+  { path: '', component: HomeComponent, pathMatch: 'full'},
+  { path: '**', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
